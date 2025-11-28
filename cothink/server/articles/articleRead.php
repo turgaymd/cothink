419f718294b7e201ada0e335a2f3a5cac0032dc9
Nesib Shirinov <sirinovnasib@gmail.com>
2025-11-28 19:56:12 +0400

    update

---
+++ cothink/server/articles/articleRead.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../db.php";
+
+$sql = "SELECT * FROM mentor_article";
+$result = $conn->query($sql);
+
+$articles = [];
+
+while ($row = $result->fetch_assoc()) {
+    $articles[] = $row;
+}
+
+echo json_encode($articles);
+?>
