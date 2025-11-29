aad0a31800f911c5eeba25510c22dc55ca3e54bd
Nesib Shirinov <sirinovnasib@gmail.com>
2025-11-29 23:18:28 +0400

    m

--- cothink/server/mentors/mentors.php
+++ cothink/server/mentors/mentors.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json");
 $sql = $conn->query("SELECT mentor_id, mentor_name, profile_img, category_id, description FROM mentors");
 $mentors = [];
 
-while($row = $sql->fetch_assoc()){
+while($row = $sql->fetch(PDO::FETCH_ASSOC)){
     $mentors[] = $row;
 }
 
