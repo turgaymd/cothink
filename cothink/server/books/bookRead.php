4405f34efecc744486488770ca429e0a464fd4da
Nesib Shirinov <sirinovnasib@gmail.com>
2025-11-30 19:17:28 +0400

    update

--- cothink/server/books/bookRead.php
+++ cothink/server/books/bookRead.php
@@ -1,13 +1,25 @@
 <?php
 require_once "../db.php";
 
+// CORS header-ləri
+header("Access-Control-Allow-Origin: http://localhost:5173"); // frontend URL
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
+
+// Preflight request üçün
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 header("Content-Type: application/json");
 
 // get all or get by id
 $id = $_GET['id'] ?? null;
 
 if ($id) {
-    $sql = "SELECT * FROM mentor_books WHERE id = ?";
+    $sql = "SELECT book_title, book_img FROM mentor_books WHERE id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->execute([$id]);
 
--- cothink/server/mentors/mentors.php
+++ cothink/server/mentors/mentors.php
@@ -3,7 +3,7 @@ require_once "../db.php";
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$sql = $conn->query("SELECT mentor_id, mentor_name, profile_img, category_id, description FROM mentors");
+$sql = $conn->query("SELECT mentor_id, mentor_name, profile_img, category_id, description, students, rating FROM mentors");
 $mentors = [];
 
 while($row = $sql->fetch(PDO::FETCH_ASSOC)){
