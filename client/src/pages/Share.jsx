da85f0cd7934bc98bcaeab44f000112da4515a13
Nesib Shirinov <sirinovnasib@gmail.com>
2025-12-02 11:41:23 +0400

    m

--- client/src/pages/Share.jsx
+++ client/src/pages/Share.jsx
@@ -1,55 +1,87 @@
-import { useState } from "react";
-import AddPost from "../components/AddPost";
-import AddCourse from "../components/AddCourse";
-import { BsBook, BsCameraVideo } from "react-icons/bs";
-import { PiArticleLight } from "react-icons/pi";
-import { LuBookAudio } from "react-icons/lu";
-const Share=()=>{
-      const [activeTab, setActiveTab]=useState("nothing")
-    return(
-        <section>
-            {activeTab==="nothing" ?
-          <>
-        <h2 className="font-bold text-black text-center text-2xl">Yeni Paylaşım Əlavə Edin</h2>
-        <p className="text-gray-400 text-center pt-2">Paylaşmaq istədiyiniz məzmunun növünü seçin</p>
-        <div className="mt-3">
-            <h2 className="font-bold text-black  text-xl pb-3">Kateqoriyalar</h2>
-            <div className="grid grid-cols-2 gap-3">
-                <div className="bg-gray-100 rounded-md share-category cursor-pointer" onClick={()=>setActiveTab("post")}>
-                    <div className="flex justify-center items-center text-blue-800 gap-5">
-                    <BsBook fontSize={32}/>
-                    <a >Post</a>
-                    </div>
+import { useRef,useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import Select from "react-select"
+const AddPost=({setActiveTab})=>{
+    const [postTitle, setPostTitle]=useState("")
+    const [postDesc,setPostDesc]=useState("")
+    const [postImg,setPostImg]=useState("")
+    const [postCategory,setPostCategory]=useState("")
+    const [postTags, setPostTags]=useState("")
+    const [error, setError]=useState("")
+
+    const fileInputRef=useRef(null);
+
+    const handleUpload=()=>{
+          fileInputRef.current.click()
+    }
+    const handlePost=async(e)=>{
+        e.preventDefault()
+          if (!postTitle || !postCategory){
+      setError("Bütün xanaları doldurunn");
+      return;
+    }
+        const newArticle={postTitle, postDesc, postImg, postCategory, postTags}
+        try{
+      const res= await axios.post("http://localhost/cothink1/cothink/server/posts/postPost.php", {newArticle},
+          { headers:{ "Content-Type":"multipart/form-data"}});
+         if(res.data.success){
+            toast.success("Post uğurla əlavə olundu")
+         }
+    }
+    catch(err){
+        console.log(err.message)
+    }
+    }
+    const options=[
+        {value:"Riyaziyyat", label:"Riyaziyyat"},
+        {value:"Fizika", label:"Fizika"},
+        {value:"Tarix", label:"Tarix"}
+      
+    ]
+    const handleSelect=(selectedCategory)=>{
+        setPostCategory(selectedCategory.value)
+    }
+    return (
+        <div className="research-form">
+            <h2 className="text-center font-bold text-3xl pb-5">Post əlavə et</h2>
+            <form className="mt-5" onSubmit={handlePost}>
+                  {error && <p className="text-center text-red-600 bg-red-50 rounded-md p-2 font-bold text-lg mb-3">{error}</p>}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                       <div>
+                <label className="block title font-semibold pb-2" htmlFor="title" >Post əlavə et</label>
+                <textarea className="w-full form-input border border-gray-300 px-3 py-1.5 outline-none rounded-lg dark:bg-gray-700 dark:text-white dark:border-none" cols={5}  rows={3}   placeholder="Post başlığı əlavə edin" onChange={(e)=>setPostTitle(e.target.value)}/>
+               </div>
+               <div>
+ <label htmlFor="description" className="block title font-semibold pb-2" >Qısa İzah / Post Haqqında</label>
+<textarea type="text" className="w-full form-input border border-gray-300 px-3 py-1.5 outline-none rounded-lg" cols={5}  rows={3}  placeholder="Post haqqında 1–2 cümləlik açıqlama" onChange={(e)=>setPostDesc(e.target.value)}/>
+</div>
                 </div>
-                      <div className="bg-gray-100 rounded-md  share-category cursor-pointer" onClick={()=>setActiveTab("course")}>
-                    <div className="flex justify-center items-center text-blue-800 gap-5">
-                     <BsCameraVideo fontSize={32}/>
-                    <a className="text-blue-800">Kurs</a>
-                    </div>
-                </div>
-                      <div className="bg-gray-100 rounded-md  share-category">
-                    <div className="flex justify-center items-center text-blue-800 gap-5">
-                    <PiArticleLight fontSize={32}/>
-                    <a className="text-blue-800" href="/addarticle" >Məqalə</a>
-                    </div>
-                </div>
-                      <div className="bg-gray-100 rounded-md  share-category">
-                    <div className="flex justify-center items-center text-blue-800 gap-5">
-                    <LuBookAudio fontSize={32}/>
-                    <a className="text-blue-800">Tədris Materialı</a>
-                    </div>
-                </div>
-            </div>
+                    <div className="mb-4">
+ <label htmlFor="category" className="block title font-semibold pb-2">Kateqoriya</label>
+<Select options={options} onChange={handleSelect} />
+
+</div>
+
+<div className="mb-4">
+     <label htmlFor="title" className="block title font-semibold pb-2">Şəkillər</label>
+     <div className="flex justify-center items-center flex-col gap-3 border border-gray-300 p-5 rounded-2xl">
+    <img src="image_icon.png"/>
+<input  ref={fileInputRef} type="file" placeholder="Şəkilləri buraya sürükləyin və ya" className="sr-only" accept="image/*" onChange={(e)=>setPostImg(e.target.files[0])}/>
+<p className="text-gray-500">Şəkilləri buraya sürükləyin və ya</p>
+    <button className="find-btn text-white bg-blue-800 rounded-md px-3 py-2 " onClick={handleUpload}>Axtar</button>
+</div>
+</div>
+<div>
+     <label htmlFor="title" className="block title font-semibold pb-2" onChange={(e)=>setPostTags(e.target.value)}>Etiketlər</label>
+<input type="text" className="w-full form-input border border-gray-300 px-3 py-2 outline-none rounded-lg" placeholder="Mövzunu ifadə edən açar sözlər əlavə edin"/>
+</div>
+<div className="submit-form mt-5 gap-3 flex flex-col md:flex-row justify-center items-center" >
+    <button className="border w-64 border-blue-800 text-blue-800 px-7 py-4" onClick={()=>setActiveTab("nothing")}>Ləğv et</button>
+    <button type="submit" className="w-64 text-white bg-blue-800 px-7 py-4">Yadda Saxla</button>
+</div>
+            </form>
         </div>
-        </> : <></>
-}
-        {
-            activeTab==="post" && <AddPost setActiveTab={setActiveTab}/>
-        }
-           {
-            activeTab==="course" && <AddCourse setActiveTab={setActiveTab}/>
-        }
-        </section>
     )
 }
-export default Share;
+export default AddPost;
